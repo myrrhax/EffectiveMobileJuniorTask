2ed597ca655932848cb13a53cd3ada47c6ff379a
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-06-07 22:58:09 +0300

    add base security config

--- src/main/java/com/example/bankcards/config/SecurityConfig.java
+++ src/main/java/com/example/bankcards/config/SecurityConfig.java
@@ -1,10 +1,20 @@
 package com.example.bankcards.config;
 
 import com.example.bankcards.config.properties.JwtProperties;
+import com.example.bankcards.security.JwtAuthenticationFilter;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
@@ -15,5 +25,27 @@ public class SecurityConfig {
     @Bean
     public SecretKey secretKey(JwtProperties jwtProperties) {
         return Keys.hmacShaKeyFor(jwtProperties.secret().getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Bean
+    @SneakyThrows
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   JwtAuthenticationFilter jwtAuthenticationFilter) {
+        return http
+                .httpBasic(HttpBasicConfigurer::disable)
+                .formLogin(FormLoginConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .exceptionHandling(config ->
+                        config.authenticationEntryPoint((req, resp, ex) -> {
+                            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                        }))
+                .sessionManagement(sessions ->
+                        sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(requestMatcher -> requestMatcher
+                        .requestMatchers("/api/v1/auth").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
     }
 }
